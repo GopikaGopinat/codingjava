a62604686ccc9f8e271f4de95bed61723e5d31f0
Gopika Gopinath <gopikagopz602@gmail.com>
2024-07-03 21:55:26 +0530

    Create numpattern1.java

---
+++ numpattern1.java
@@ -0,0 +1,16 @@
+package javaProgram;
+
+public class Pattern {
+	public static void main(String[] args) {
+		int i,n=5,j;
+		for(i=1;i<=n;i++)
+		{
+			for(j=1;j<=i;j++)
+			{
+			System.out.print(j+" ");
+		}
+			System.out.println();
+
+	}
+	}
+}
