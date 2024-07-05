d018b31b008d38cfa2d6b5fc6cc8cd08061d7f99
Gopika Gopinath <gopikagopz602@gmail.com>
2024-07-05 21:27:53 +0530

    Create swap.java

---
+++ swap.java
@@ -0,0 +1,13 @@
+package javaprogram;
+
+public static void main(String[] args) {
+		int a=25,b=35,temp;
+		System.out.println("Before swapping");
+		System.out.println("a = "+a +" "  + "b= "+b);
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println("After swapping");
+		System.out.println("a= "+a +" "  +"b= "+b);	
+	}
+}
