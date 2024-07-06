ec053e2002709291ad8da91f2021c8cee9dd22cd
Gopika Gopinath <gopikagopz602@gmail.com>
2024-07-06 21:38:13 +0530

    Create function2.java

---
+++ function2.java
@@ -0,0 +1,15 @@
+package javaProgram;
+public class SumAdd {
+	public static int add()
+	{
+		int a=10,b=5,sum;
+		sum=a+b;
+		return sum;	
+	}
+	public static void main(String args[])
+	{
+		int c;
+		c=SumAdd.add();
+		System.out.println("Sum is "+c);
+	}
+}
