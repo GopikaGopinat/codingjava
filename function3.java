dff864a63c4653837f00292c655d5a9a5f06fb33
Gopika Gopinath <gopikagopz602@gmail.com>
2024-07-07 20:18:34 +0530

    Create function3.java

---
+++ function3.java
@@ -0,0 +1,13 @@
+package javaProgram;
+public class SumAdd {
+	public static void add(int a,int b)
+	{
+		int sum;
+		sum=a+b;
+		System.out.println("Sum is "+sum);
+	}
+	public static void main(String args[])
+	{
+		SumAdd.add(20,5);
+	}
+}
