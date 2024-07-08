20295ef9657b3076409378ccd8dbc0b5ab8e65f8
Gopika Gopinath <gopikagopz602@gmail.com>
2024-07-08 22:57:39 +0530

    Create function4.java

---
+++ function4.java
@@ -0,0 +1,14 @@
+package javaProgram;
+public class SumAdd {
+	public static int add(int a,int b)
+	{
+		int sum=a+b;
+		return sum;
+	}
+	public static void main(String args[])
+	{
+		int c;
+		c=SumAdd.add(30,20);
+		System.out.println("Sum is "+c);
+	}
+}
